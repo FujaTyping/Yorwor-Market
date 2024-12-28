4e18bddc9730da36b727b407596d12f1fb13dbce
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-12-28 22:34:53 +0700

    Init elysia

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { Elysia } from "elysia";
+import { swagger } from '@elysiajs/swagger'
+
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => "Hello Elysia").listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
