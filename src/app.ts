b73e886486336fb891cbb4fbf7ceaa283e4eb9f4
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2025-01-11 16:09:22 +0700

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { autoload } from "elysia-autoload";
 import { rateLimit } from 'elysia-rate-limit'
+import { cors } from '@elysiajs/cors'
 
 const PORT = 3000;
 
 const app = new Elysia()
+    .use(cors())
     .use(swagger())
     .use(await autoload())
     .use(rateLimit())
